c4d82de2a0827342d320fdb497181c210a7eefb6
Nazar <exxper.dev@gmail.com>
2021-03-07 14:24:33 +0200

    Update token

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,2 +1,2 @@
 export const BASE_URL = 'https://api.github.com';
-export const GITHUB_TOKEN = '9bf3137795409f621d49110054be10a91c495930';
+export const GITHUB_TOKEN = '5b2f604351604f2da6e824a586f1a2d52131cb10';
