6c5f795816baf27df06b9b2eef8467e3918b2880
Nazar <exxper.dev@gmail.com>
2021-03-07 17:12:19 +0200

    Add token to env

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,2 +1,2 @@
 export const BASE_URL = 'https://api.github.com';
-export const GITHUB_TOKEN = 'c25047ae503f5f56d8bcf8581d861a01d2c29fac';
+export const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
